1e72d4cd175031cd02e348e72ef389630c2de185
Brett Viren <bv@bnl.gov>
2012-07-17 14:20:23 -0400

    Turn back on VGM code.

--- people/bv/cowbells/src/Cowbells/BuildFromRoot.cc
+++ people/bv/cowbells/src/Cowbells/BuildFromRoot.cc
@@ -1,7 +1,7 @@
 #include "Cowbells/BuildFromRoot.h"
 
-// #include <RootGM/volumes/Factory.h>
-// #include <Geant4GM/volumes/Factory.h>
+#include <RootGM/volumes/Factory.h>
+#include <Geant4GM/volumes/Factory.h>
 
 #include <TGeoManager.h>
 #include <G4VPhysicalVolume.hh>
@@ -22,16 +22,14 @@ G4VPhysicalVolume* Cowbells::BuildFromRoot::Construct()
 
     int debug_level = 0;        // 0=shut up, 1=info, 2=verbose
 
-    // // Import geometry from Root to VGM
-    // RootGM::Factory rtFactory;
-    // rtFactory.SetDebug(0);
-    // rtFactory.Import(geo->GetTopNode());
+    // Import geometry from Root to VGM
+    RootGM::Factory rtFactory;
+    rtFactory.SetDebug(0);
+    rtFactory.Import(geo->GetTopNode());
 
-    // // Export VGM geometry to Geant4
-    // Geant4GM::Factory g4Factory;
-    // g4Factory.SetDebug(0);
-    // rtFactory.Export(&g4Factory);
-    // return g4Factory.World();
-
-    return 0;
+    // Export VGM geometry to Geant4
+    Geant4GM::Factory g4Factory;
+    g4Factory.SetDebug(0);
+    rtFactory.Export(&g4Factory);
+    return g4Factory.World();
 }
