b260de4dcc018ecc1b8a8fe017ce8993f5493c00
Brett Viren <bv@bnl.gov>
2013-02-13 16:44:21 -0500

    Include box material in the file pattern.

--- python/cowbells/ana/boxes/__init__.py
+++ python/cowbells/ana/boxes/__init__.py
@@ -6,7 +6,7 @@ Check some things with the NSRL setup
 from params import get as params
 
 from cowbells.ana.run import BaseRun
-BaseRun.file_pattern = '%(study)s-%(label)s-%(sample)s-%(particle)s-%(energy)sMeV-%(nevents)sevts'
+BaseRun.file_pattern = '%(study)s-%(label)s-%(box)s-%(sample)s-%(particle)s-%(energy)sMeV-%(nevents)sevts'
 
 from cowbells.ana.run import ConfigSingleBoxRun as ConfigRun
 from cowbells.ana.run import SimRun
